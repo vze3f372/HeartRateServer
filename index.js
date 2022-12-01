ccb49da682d20f2021be08fc369fd877eba52be0
vze3f372 <joe.hotchkiss@gmail.com>
2022-12-01 21:12:05 +0200

    Migrating from github

--- index.js
+++ index.js
@@ -1,68 +1,135 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const {WebSocketServer} = require('ws');
-const http = require('http');
-const express = require('express');
-const mqtt = require('mqtt');
-const sqlite3 = require('sqlite3');
+const path = require("path");
+const {WebSocketServer} = require("ws");
+const http = require("http");
+const express = require("express");
+const mqtt = require("mqtt");
+const sqlite3 = require("sqlite3");
 const app = express();
 const db = new sqlite3.Database(path.resolve(__dirname, "patient"));
-let bkrAddr = "mqtt://10.0.0.4:1883"//18.198.188.151:21883";//10.0.0.4:1883"
+let bkrAddr = "mqtt://10.0.0.4:1883"; //18.198.188.151:21883";//10.0.0.4:1883"
 let clntID = "joe";
 let topic = "Joe/HR";
 let debug = "debug";
 let port = 3003;
-let client = mqtt.connect(bkrAddr, {clientId:clntID});
+let client = mqtt.connect(bkrAddr, {clientId: clntID});
 let server = http.createServer(app).listen(port);
 
+app.use("/static", express.static(path.resolve(__dirname, "views")));
+app.use("/static", express.static(path.resolve(__dirname, "style")))
+app.use(express.static("static"));
 
-app.use('/static', express.static(path.resolve(__dirname, 'views')))
 
 const wss = new WebSocketServer({
     server: server,
-    clientTracking: true
+    clientTracking: true,
 });
 
 wss.on("connection", (ws) => {
     console.log("WebSocket Connected");
-    ws.on("message", (message)=> {
+    ws.on("message", (message) => {
         console.log(message.toString());
     });
 });
 
-client.on("connect",() => {
+client.on("connect", () => {
     console.log("MQTT connected");
     client.subscribe(topic);
-    console.log("MQTT subscribed to topic: " + topic)
+    client.subscribe("history");
+    console.log("MQTT subscribed to topic: " + topic);
 });
 
-client.on("error",error => {
+client.on("error", (error) => {
     console.log("Can't connect" + error);
-    process.exit(1)
+    process.exit(1);
 });
 
-client.on('message', (topic, message, packet) => {
+client.on("message", (topic, message, packet) => {
     let pData = JSON.parse(message);
-    db.run("insert into pData(pName, mDate, mTime, bpm)values ($pName, $mDate, $mTime, $bpm)", {
-        $pName:pData.pName,
-        $mDate:pData.mDate,
-        $mTime:pData.mTime,
-        $bpm:pData.bpm
-    });
-    for (const ws of wss.clients){
+    console.log(pData);
+    db.run(
+        "insert into pData(pName, mDate, mTime, bpm)values ($pName, $mDate, $mTime, $bpm)",
+        {
+            $pName: pData.pName,
+            $mDate: pData.mDate,
+            $mTime: pData.mTime,
+            $bpm: pData.bpm
+        });
+    for (const ws of wss.clients) {
         ws.send(message.toString());
     }
-    client.publish(debug, pData.pName.toString());
 });
 
-app.get('/', (req, res) => {
-    db.get("select pName, mDate, mTime, bpm from pData order by id desc limit 1;", (err, current) => {
-        let pData = Object.assign({}, current);
-        res.render(path.resolve(__dirname,'views/home.ejs'), pData);
-    });
+app.get("/", (req, res) => {
+    const query = 'CREATE TABLE IF NOT EXISTS tData (id INTEGER PRIMARY KEY AUTOINCREMENT, pName STRING, mDate STRING NOT NULL, mTime STRING NOT NULL, bpm INT NOT NULL);'
+    db.run(query);
+    db.get(
+        "select pName, mDate, mTime, bpm from pData order by id desc limit 1;",
+        (err, current) => {
+            const pData = []
+            if (current === undefined) {
+                pData[0] = {
+                    "pName": "",
+                    "mDate": "",
+                    "mTime": "",
+                    "bpm": 0
+                };
+            } else {
+                pData.push(current);
+            }
+            console.log(current);
+            //client.publish(topic, JSON.stringify(pData));
+            console.log(pData);
+            res.render(path.resolve(__dirname, "views/home.ejs"), pData[0]);
+        }
+    );
 });
 
-app.get('/home.js', (req, res) => res.sendFile(path.resolve(__dirname, 'views/home.js')));
-app.get('/data.js', (req, res) => res.sendFile(path.resolve(__dirname, 'views/data.js')));
-app.listen(3002);
+app.get("/history", (req, res) => {
+    res.render(path.resolve(__dirname, "views/history.ejs"));
+});
+
+app.get("/historydata", async (req, res) => {
+    try {
+        console.log("fetching data");
+        db.all(
+            "select pName, mDate, mTime, bpm from pData order by id asc ;",
+            (err, current) => {
+                let data = current;
+                res.json(data);
+            }
+        );
+    } catch (e) {
+        console.log(e);
+        res.status(404).send(e);
+    }
+});
+
+app.delete("/clear", (req, res) => {
+    try {
+        db.run("DELETE FROM pData;", (err) => {
+            res.status(200);
+        });
+    } catch (e) {
+        res.status(204).send(e);
+    }
+});
+
+app.delete("/deletelast", (req, res) => {
+    db.run("DELETE FROM pData WHERE id = (SELECT MAX(id)) FROM pData");
+});
+
+app.get("/home.js", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "views/home.js"))
+);
+
+app.get("/data.js", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "views/data.js"))
+);
+
+app.get("/style.css", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "style/style.css"))
+);
+
+app.listen(3000);
--- views/home.js
+++ views/home.js
@@ -1,48 +1,69 @@
-'use strict';
+"use strict";
+
+const ctx = document.getElementById("myChart");
+const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [],
+        datasets: [
+            {
+                label: "Heart Rate",
+                data: [],
+                borderColor: 'rgb(143,8,8)',
+                backgroundColor: 'rgb(143,8,8)',
+                borderWidth: 2,
+            },
+        ],
+    },
+    options: {
+        tickColor: (45, 44, 44, 0.9),
+        backdropColor: (45, 44, 44, 0.9),
+        aspectRatio: 3,
+        maintainAspectRatio: true,
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
 
 const socket = new WebSocket(`ws://localhost:3003`);
-socket.addEventListener('open', ()=> {
+socket.addEventListener("open", (message) => {
     socket.send("WS Client Connected!");
-    const ctx = document.getElementById('myChart');
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [{
-                label: '# of Votes',
-                data: [],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
+    socket.addEventListener("message", (event) => {
+        const pName = JSON.parse(event.data).pName;
+        const mDate = JSON.parse(event.data).mDate;
+        const mTime = JSON.parse(event.data).mTime;
+        const bpm = JSON.parse(event.data).bpm;
+        const name = document.querySelector(".name");
+        const date = document.querySelector(".date");
+        const hr = document.querySelector(".hr");
+        name.innerText = "Patient name: " + pName;
+        date.innerText = "Measurement date: " + mDate;
+        hr.innerText = "Heart rate: " + bpm;
+        socket.send("Message received");
+        // socket.send(JSON.parse(event.data).toString);
+
+        //chart = document.getElementById('myChart');
+        addData(chart, mDate, bpm);
     });
 });
 
-socket.addEventListener("message", event => {
-    const pName = JSON.parse(event.data).pName;
-    const mDate = JSON.parse(event.data).mDate
-    const bpm = JSON.parse(event.data).bpm;
-    const name = document.querySelector(".name");
-    const date = document.querySelector(".date");
-    const hr = document.querySelector(".hr");
-    name.innerText = "Patient name: " + pName;
-    date.innerText = "Measurement date: " + mDate;
-    hr.innerText = "Heart rate: " + bpm;
-    socket.send("client" + event.data.toString());
-    const ctx = document.getElementById('myChart');
-    addData(ctx, mDate, bpm);
-});
+(() => {
+    fetch('/historydata')
+        .then(res => res.json())
+        .then(pData => {
+            console.log(pData.length);
+            for (const data of pData) {
+                //console.log(datum);
+                addData(chart, data.mTime, data.bpm);
+            }
+        });
+}).call({});
 
 function addData(chart, label, data) {
     chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
+    chart.data.datasets[0].data.push(data);
     chart.update();
 }
